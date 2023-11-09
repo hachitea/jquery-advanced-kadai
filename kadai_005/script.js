1a44376f5fe27fe868f9d7183e851efd0489d540
hachitea <mitsugohachimu@gmail.com>
2023-11-09 17:50:04 +0900

    kadai_005を追加

---
+++ kadai_005/script.js
@@ -0,0 +1,77 @@
+$(function () {
+
+  //ボタンアニメーション
+  $('.button-more').on('mouseover', function () {
+    $(this).animate({
+      opacity: 0.5,
+      marginLeft: 20,
+    }, 100);
+  });
+  $('.button-more').on('mouseout', function () {
+    $(this).animate({
+      opacity: 1.0,
+      marginLeft: 0,
+    }, 100);
+  });
+
+  //カルーセル
+  $('.carousel').slick({
+    autoplay: true,
+    dots: true,
+    infinite: true,
+    autoplaySpeed: 5000,
+    arrows: false,
+  });
+
+  //AjaxでSTATIC FORMにデータを送信
+  $('#submit').on('click', function (event) {
+    //formタグによる送信を拒否
+    event.preventDefault();
+
+    //入力チェックをした結果、エラーがあるかないか判定
+    let result = inputCheck();
+  });
+
+  //フォーカスが外れたとき（blur）にフォームの入力チェックをする
+  $('#name').blur(function () {
+    inputCheck();
+  });
+  $('#furigana').blur(function () {
+    inputCheck();
+  });
+  $('#email').blur(function () {
+    inputCheck();
+  });
+  $('#tel').blur(function () {
+    inputCheck();
+  });
+  $('#message').blur(function () {
+    inputCheck();
+  });
+  $('#agree').blur(function () {
+    inputCheck();
+  });
+
+  //お問い合わせフォームの入力チェック
+  function inputCheck() {
+    //エラーのチェック結果
+    let result;
+
+    //エラーメッセージのテキスト
+    let message = '';
+
+    //エラーがなければfalse、エラーがあればtrue
+    let error = false;
+
+    //お名前のチェック
+    if ($('#name').val() == '') {
+      //エラーあり
+      $('#name').css('background=color', '#f79999');
+      error = true;
+      message += 'お名前を入力してください。/n';
+    } else {
+      //エラーなし
+      $('#name').css('background-color', '#fafafa');
+    }
+  }
+});
